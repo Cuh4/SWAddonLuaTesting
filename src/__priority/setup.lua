44f563ee9e212b35c4b779a6f33804a51a4ada51
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-01-02 08:49:24 +0000

    new file that sets up things

---
+++ src/__priority/setup.lua
@@ -0,0 +1,23 @@
+--------------------------------------------------------
+-- [Main] Priority - Setup
+--------------------------------------------------------
+
+--[[
+    ----------------------------
+
+    INFO:
+        This tests if the framework's internal "artificialOnTick" event is working correctly.
+
+    CREDIT:
+        Author: @cuh6_ (Discord)
+        GitHub Repository: https://github.com/Cuh4/SWAddonLuaTesting
+        Created: 02/01/2024
+
+    ----------------------------
+]]
+
+-------------------------------
+-- // Setup
+-------------------------------
+-- Create logger
+AuroraFramework.services.debuggerService.createLogger("Main", true)
