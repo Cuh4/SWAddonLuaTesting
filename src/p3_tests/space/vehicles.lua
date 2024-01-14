4943b9affe106f05fc54663ac003cbb2367d939f
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-01-14 12:41:10 +0000

    test whether or not vehicles can be spawned and manipulated in space

---
+++ src/p3_tests/space/vehicles.lua
@@ -0,0 +1,66 @@
+--------------------------------------------------------
+-- [Main] Space - Vehicles
+--------------------------------------------------------
+
+--[[
+    ----------------------------
+
+    INFO:
+        This tests if Stormworks supports spawning vehicles in space.
+
+    CREDIT:
+        Author: @cuh6_ (Discord)
+        GitHub Repository: https://github.com/Cuh4/SWAddonLuaTesting
+        Created: 14/01/2024
+
+    ----------------------------
+]]
+
+-------------------------------
+-- // Variables
+-------------------------------
+g_savedata.count = g_savedata.count or 0
+
+-------------------------------
+-- // Main
+-------------------------------
+-- // register commands
+-- teleport to space
+AuroraFramework.services.commandService.create(function(command, args, player)
+    local pos = matrix.translation(
+        tonumber(args[1]) or 0,
+        tonumber(args[2]) or 0,
+        tonumber(args[3]) or 0
+    )
+
+    player:teleport(server.getAstroPos(pos))
+end, "tp", {"t"})
+
+-- spawn group
+AuroraFramework.services.commandService.create(function(command, args, player)
+    -- spawn group
+    local pos = player:getPosition()
+
+    local group = AuroraFramework.services.groupService.spawnGroup(
+        AuroraFramework.libraries.matrix.offset(pos, 0, 0, 10),
+        1
+    )
+
+    -- randomly move it around its spawn point
+    AuroraFramework.services.timerService.loop.create(0.1, function(loop)
+        -- check if group despawned
+        if not AuroraFramework.services.groupService.getGroup(group.properties.group_id) then
+            return loop:remove()
+        end
+
+        -- randomly teleport group
+        group:teleport(
+            AuroraFramework.libraries.matrix.randomOffset(pos, 100, true)
+        )
+    end)
+
+    -- despawn the group after some time
+    AuroraFramework.services.timerService.delay.create(5, function()
+        group:despawn()
+    end)
+end, "spawn", {"s"})
