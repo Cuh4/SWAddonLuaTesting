1b3bbd7a5e4c0a442d9d353eed6bd003a834b635
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-01-14 15:31:03 +0000

    add main file

---
+++ src/main.lua
@@ -0,0 +1,25 @@
+--------------------------------------------------------
+-- [Main] SW Addon Lua Testing
+--------------------------------------------------------
+
+--[[
+    ----------------------------
+
+    INFO:
+        This addon mainly tests my framework @ https://github.com/Cuh4/AuroraFramework, as well as the game's addon API.
+
+    CREDIT:
+        Author: @cuh6_ (Discord)
+        GitHub Repository: https://github.com/Cuh4/SWAddonLuaTesting
+        Created: 14/01/2024
+
+    ----------------------------
+]]
+
+-------------------------------
+-- // Functions
+-------------------------------
+---@param pos SWMatrix
+function isInSpace(pos)
+    return pos[14] >= 190000
+end
